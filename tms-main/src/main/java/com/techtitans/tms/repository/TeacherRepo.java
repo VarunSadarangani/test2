8da2e997af1af51d97a58ad1fdc56860cbf4e7a3
VarunSadarangani <saadarangani7@gmail.com>
2024-09-29 12:26:43 +0530

    Add files via upload

---
+++ tms-main/src/main/java/com/techtitans/tms/repository/TeacherRepo.java
@@ -0,0 +1,11 @@
+package com.techtitans.tms.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.techtitans.tms.model.Teachers;
+
+@Repository
+public interface TeacherRepo extends JpaRepository<Teachers,Integer> {
+
+}
